24810e6571f62827d670f833f198c06c61f2c7fb
Will <william.murphy@optispark.ai>
2025-06-27 14:39:34 +0100

    rearranged endpoint order

--- main.py
+++ main.py
@@ -62,6 +62,37 @@ async def add_author(author: Author) -> AuthorId:
     return id
 
 
+@app.get("/author/{author_id}")
+async def get_author(author_id: AuthorId) -> Author:
+    if author_id not in author_db:
+        raise HTTPException(status_code=400, detail=f"Author with ID {author_id} not found")
+
+    return author_db[author_id]
+
+
+@app.put("/author/{author_id}")
+async def update_author(author_id: AuthorId, author: Author) -> AuthorId:
+    if author_id not in author_db:
+        raise HTTPException(status_code=400, detail=f"Author with ID {author_id} not found")
+
+    author_db[author_id] = author
+    return author_id
+
+
+@app.delete("/author/{author_id}")
+async def delete_author(author_id: AuthorId) -> AuthorId:
+    if author_id not in author_db:
+        raise HTTPException(status_code=400, detail=f"Author with ID {author_id} not found")
+    elif len(author_db[author_id].book_ids) > 0:
+        raise HTTPException(
+            status_code=409,
+            detail=f"Cannot delete author {author_id}: Author has {len(author_db[author_id].book_ids)} books associated"
+        )
+
+    del author_db[author_id]
+    return author_id
+
+
 @app.get("/book/")
 async def get_books() -> list[BookResponse]:
     return [BookResponse(BookId=book_id, **book.model_dump())
@@ -83,29 +114,12 @@ async def add_book(book: Book) -> BookId:
     return book_id
 
 
-@app.get("/author/{author_id}")
-async def get_author(author_id: AuthorId) -> Author:
-    if author_id not in author_db:
-        raise HTTPException(status_code=400, detail=f"Author with ID {author_id} not found")
-
-    return author_db[author_id]
-
-
 @app.get("/book/{book_id}")
 async def get_book(book_id: BookId) -> Book:
     if book_id not in book_db:
         raise HTTPException(status_code=400, detail=f"Book with ID {book_id} not found")
 
     return book_db[book_id]
-
-
-@app.put("/author/{author_id}")
-async def update_author(author_id: AuthorId, author: Author) -> AuthorId:
-    if author_id not in author_db:
-        raise HTTPException(status_code=400, detail=f"Author with ID {author_id} not found")
-
-    author_db[author_id] = author
-    return author_id
 
 
 @app.put("/book/{book_id}")
@@ -115,20 +129,6 @@ async def update_book(book_id: BookId, book: Book) -> BookId:
 
     book_db[book_id] = book
     return book_id
-
-
-@app.delete("/author/{author_id}")
-async def delete_author(author_id: AuthorId) -> AuthorId:
-    if author_id not in author_db:
-        raise HTTPException(status_code=400, detail=f"Author with ID {author_id} not found")
-    elif len(author_db[author_id].book_ids) > 0:
-        raise HTTPException(
-            status_code=409,
-            detail=f"Cannot delete author {author_id}: Author has {len(author_db[author_id].book_ids)} books associated"
-        )
-
-    del author_db[author_id]
-    return author_id
 
 
 @app.delete("/book/{book_id}")
